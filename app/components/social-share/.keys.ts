5049259ad5685e8e1c73938b79b7377364e9d6ad
bryantgillespie <hey@bryantgillespie.com>
2024-11-22 16:32:41 -0500

    remove other

--- app/components/social-share/.keys.ts
+++
@@ -1,3 +0,0 @@
-import type { InjectionKey, Ref } from "vue";
-
-export default Symbol() as InjectionKey<Ref<string>>;
--- server/utils/hash.ts
+++
@@ -1,8 +0,0 @@
-import { createHash } from 'node:crypto';
-
-export function createVisitorHash(ip: string, salt: string): string {
-	return createHash('sha256')
-		.update(ip + salt)
-		.digest('hex')
-		.slice(0, 32);
-}
