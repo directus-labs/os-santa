f384985e513e8ac89d1c385fa256710f191ef78a
bryantgillespie <hey@bryantgillespie.com>
2024-11-22 16:43:20 -0500

    sample favicons

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -4,7 +4,7 @@ useHead({
 		{ charset: 'utf-8' },
 		{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 	],
-	link: [{ rel: 'icon', href: '/favicon.svg' }],
+	link: [{ rel: 'icon', href: '/favicon.ico' }],
 	htmlAttrs: {
 		lang: 'en',
 	},
