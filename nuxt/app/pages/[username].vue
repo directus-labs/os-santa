005a9610ba2337bd0694e16ed22b0654a28f10e0
bryantgillespie <hey@bryantgillespie.com>
2024-11-26 05:25:34 -0500

    clean up page component

--- nuxt/app/pages/[username].vue
+++ nuxt/app/pages/[username].vue
@@ -47,7 +47,7 @@ defineOgImageComponent('Username', {
 				</BaseText>
 			</div>
 
-			<div class="max-w-sm mx-auto">
+			<div class="relative max-w-sm mx-auto">
 				<div class="relative w-full">
 					<NiceGauge :list="list" class="w-full mx-auto relative" @animation-completed="showLetter = true" />
 					<div class="absolute bottom-11 w-full justify-center flex items-center">
@@ -70,25 +70,10 @@ defineOgImageComponent('Username', {
 				<UButtonGroup size="xl" class="w-full mt-4 relative">
 					<UInput :value="currentUrl" readonly class="font-mono w-full" />
 					<UButton icon="uil:copy" variant="solid" color="primary" @click="() => copyUrl()" />
-					<!-- Copied Alert -->
-					<Transition
-						enter-active-class="transition-all duration-300"
-						leave-active-class="transition-all duration-300"
-						mode="out-in"
-						enter-from-class="opacity-0"
-						leave-to-class="opacity-0"
-					>
-						<UAlert
-							v-if="copied"
-							title="Copied"
-							description="Link copied to clipboard!"
-							class="absolute -top-1/2"
-						/>
-					</Transition>
 				</UButtonGroup>
 			</div>
-			<Page class="relative pt-12">
-				<template #left>
+			<div class="relative pt-12 wrapper">
+				<aside class="left">
 					<!-- Share Buttons -->
 					<div class="sticky top-20 mt-12">
 						<p class="text-red-200 text-2xl text-center font-cursive -rotate-2">Sharing is Caring</p>
@@ -98,9 +83,29 @@ defineOgImageComponent('Username', {
 							<SocialShareLinkedIn class="hover:text-green-500" />
 						</SocialShare>
 					</div>
-				</template>
+				</aside>
 
-				<template #right>
+				<div class="content">
+					<SantaLetterPaper v-show="showLetter" class="max-w-3xl mx-auto letter-unfold mt-4">
+						<img src="/images/os-santa.svg" class="absolute bottom-0 right-0 w-64 -mb-32 z-10" />
+						<div v-if="status === 'pending'" class="text-center py-4">
+							<p class="text-gray-600 mt-2">Loading your letter...</p>
+						</div>
+						<div v-else-if="error" class="text-center text-red-600 py-4">
+							Oops! Something went wrong loading the letter.
+						</div>
+						<div v-else-if="data" class="relative w-full">
+							<div class="relative z-10 prose prose-2xl text-gray-900 text-3xl font-cursive" v-html="letter" />
+							<p class="relative z-10 prose prose-2xl text-gray-900 text-3xl font-cursive">
+								Sarcastically yours,
+								<br />
+								Salty Open Source Santa
+							</p>
+						</div>
+					</SantaLetterPaper>
+				</div>
+
+				<aside class="right">
 					<div class="sticky top-20 mt-12">
 						<!-- Like Button -->
 						<ClientOnly>
@@ -113,24 +118,8 @@ defineOgImageComponent('Username', {
 							/>
 						</ClientOnly>
 					</div>
-				</template>
-
-				<SantaLetterPaper v-show="showLetter" class="max-w-3xl mx-auto letter-unfold mt-4">
-					<img src="/images/os-santa.svg" class="absolute bottom-0 right-0 w-64 -mb-32 z-10" />
-
-					<div v-if="status === 'pending'" class="text-center py-4">
-						<p class="text-gray-600 mt-2">Loading your letter...</p>
-					</div>
-
-					<div v-else-if="error" class="text-center text-red-600 py-4">
-						Oops! Something went wrong loading the letter.
-					</div>
-
-					<div v-else-if="data" class="relative w-full">
-						<div class="relative z-10 prose prose-2xl text-gray-900 text-3xl font-cursive" v-html="letter" />
-					</div>
-				</SantaLetterPaper>
-			</Page>
+				</aside>
+			</div>
 		</UContainer>
 	</div>
 </template>
@@ -138,15 +127,6 @@ defineOgImageComponent('Username', {
 <style>
 .text-shadow {
 	text-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
-}
-.bg-gold {
-	background-color: #d4af37;
-}
-.text-gold {
-	color: #d4af37;
-}
-.border-gold {
-	border-color: #d4af37;
 }
 
 @keyframes unfold {
@@ -168,7 +148,32 @@ defineOgImageComponent('Username', {
 	transform-style: preserve-3d;
 }
 
-.string {
-	pointer-events: none;
+
+.wrapper {
+	display: flex;
+	flex-direction: column;
+	position: relative;
+}
+
+@media (min-width: 64rem) {
+	.wrapper {
+		display: grid;
+		grid-template-columns: 1fr 4fr 1fr;
+		grid-column-gap: 32px;
+	}
+
+	.left {
+		grid-column: 1;
+		position: relative;
+	}
+
+	.right {
+		grid-column: 3;
+		position: relative;
+	}
+
+	.content {
+		grid-column: 2;
+	}
 }
 </style>
