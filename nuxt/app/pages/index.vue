bee6c395523bef0cc120797b1026ae5d2c874316
bryantgillespie <hey@bryantgillespie.com>
2024-12-07 08:46:05 -0500

    overflow fix

--- nuxt/app/pages/index.vue
+++ nuxt/app/pages/index.vue
@@ -176,7 +176,7 @@ const copy = {
 					<UButton class="mt-4">How does this work?</UButton>
 					<template #content>
 						<UContainer>
-							<div v-html="copy.help" class="prose py-8 md:py-16 overflow-y-auto"></div>
+							<div v-html="copy.help" class="prose py-8 md:py-16 flex flex-col max-h-[400px] overflow-y-auto"></div>
 						</UContainer>
 					</template>
 				</UDrawer>
