40d824e3eb45d20f73a0ed053799491468fb20f3
bryantgillespie <hey@bryantgillespie.com>
2025-12-12 14:15:54 -0500

    fix images

--- nuxt/app/components/PoweredBy.vue
+++ nuxt/app/components/PoweredBy.vue
@@ -1,9 +1,17 @@
 <template>
 	<div class="relative p-4 pb-24 lg:pb-12">
-		<img src="/images/snows.png" alt="Snow Scene" class="absolute bottom-0 right-0 w-full object-cover z-0" />
+		<img
+			src="/images/snows.png"
+			alt="Snow Scene"
+			class="absolute bottom-0 right-0 w-full object-cover z-0 pointer-events-none"
+		/>
 
 		<!-- Santa Bunny on top of snow scene -->
-		<img src="/images/santa-bunny.png" alt="Santa Bunny" class="absolute bottom-0 left-1/4 w-48 md:w-64 lg:w-72 z-10" />
+		<img
+			src="/images/santa-bunny.png"
+			alt="Santa Bunny"
+			class="absolute bottom-0 left-1/4 w-48 md:w-64 lg:w-72 z-10 pointer-events-none"
+		/>
 
 		<p
 			class="relative z-20 text-center text-white text-shadow text-2xl flex justify-center items-center gap-2 font-bold"
